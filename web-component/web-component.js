37c7269c20b59ad0b4f2e3090f028566f892cc48
LeeJams <leejam208@gmail.com>
2022-01-02 23:44:39 +0900

    웹컴포넌트

--- web-component/web-component.js
+++ web-component/web-component.js
@@ -1,34 +1,48 @@
-class CustomInput extends HTMLElement{
-
-  //컴포넌트 만드는 틀
+class CurrentTime extends HTMLElement {
+  constructor() {
+      // 클래스 초기화. 속성이나 하위 노드는 접근할 수는 없다.
+      super();
+  }
+  static get observedAttributes() {
+      // 모니터링 할 속성 이름
+      console.log("observed");
+      return ['locale'];
+  }
   connectedCallback() {
-
-    this.render();
-
+      // DOM에 추가되었다. 렌더링 등의 처리를 하자.
+      console.log("connect");
+      this.start();
   }
-  render(){
-    let main = document.createElement('main');
-
-    let label = document.createElement('label');
-    label.innerHTML = '이름';
-    main.appendChild(label);
-
-    let input = document.createElement('input');
-    main.appendChild(input);
-
-    this.appendChild(main);
+  disconnectedCallback() {
+      // DOM에서 제거되었다. 엘리먼트를 정리하는 일을 하자.
+      console.log("disconnect");
+      this.stop();
   }
-
-  //재랜더링 기능
-  static get observedAttributes(){
-    return ['name']
+  attributeChangedCallback(attrName, oldVal, newVal) {
+      // 속성이 추가/제거/변경되었다.
+      console.log("attributeChange");
+      this[attrName] = newVal;
   }
-
-  attributeChangedCallback(){
-    this.render
+  adoptedCallback(oldDoc, newDoc) {
+      // 다른 Document에서 옮겨져 왔음
+      // 자주 쓸 일은 없을 것.
   }
-
-  //Lit = Web component 쉬운 문법으로 만들어줌
+  start() {
+      // 필요에 따라 메서드를 추가할 수 있다.
+      // 이 클래스 인스턴스는 HTMLElement이다.
+      // 따라서 `document.querySelector('current-time').start()`로 호출할 수 있다.
+      this.stop();
+      this._timer = window.setInterval(() => {
+        this.innerText = new Date().toLocaleString(this.locale);
+      }, 1000);
+  }
+  stop() {
+      // 이 메서드 역시 CurrentTime클래스의 필요에 의해 추가했다.
+      if (this._timer) {
+          window.clearInterval(this._timer);
+          this._timer = null;
+      }
+  }
 }
-
-customElements.define('custom-input', CustomInput);
+// <current-time> 태그가 CurrentTime 클래스를 사용하도록 한다.
+customElements.define('current-time', CurrentTime);
