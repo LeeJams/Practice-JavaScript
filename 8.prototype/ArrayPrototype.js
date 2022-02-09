5a790e617aade7beeae9ea46d1e82392a1e7d022
LeeJams <89899636+LeeJams@users.noreply.github.com>
2022-02-09 23:41:47 +0900

    array prototype

--- 8.prototype/ArrayPrototype.js
+++ 8.prototype/ArrayPrototype.js
@@ -4,6 +4,11 @@ const arr = ['Lee', 'Kim', 'Park'];
 
 const newArr = arr.map(n => n + '1'); // ['Lee1', 'Kim1', 'Park1']
 const newArr2 = arr.concat('Cho'); // ['Lee', 'Kim', 'Park', 'Cho']
+const newArr3 = arr.filter(n => n.startsWith('L'));
 
 console.dir(arr);
 console.log(arr.add());
+
+console.log(newArr);
+console.log(newArr2);
+console.log(newArr3);
